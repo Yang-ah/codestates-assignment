6e35f94ac516b32540ad2711956990a6de46f322
kyoongdev <9898junjun2@gmail.com>
2023-02-21 11:57:00 +0900

    feature : ajax-practice

--- react_20230221/src/api/Post/index.js
+++
@@ -1,5 +0,0 @@
-import apiClient from "../apiClient";
-
-export const getPosts = () => {
-  return apiClient.get("/posts");
-};
--- react_20230221/src/pages/Home/index.js
+++ react_20230221/src/pages/Home/index.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { getProducts } from "../../api/Products";
+
 import styles from "./home.module.scss";
 
 const Home = () => {
+  const [count, setCount] = useState(0);
+
+  const onClick = () => {
+    setCount(count + 1);
+  };
+
+  const onGetItem = async () => {
+    const response = await getProducts();
+  };
+
   return (
     <div class={styles.wrapper}>
-      <div class={styles.item}>A</div>
+      <button onClick={onClick}>Up</button>
+      <button onClick={onGetItem}>API 호출</button>
+      {/* <div class={styles.item}>A</div>
       <div class={styles.item}>B</div>
       <div class={styles.item}>C</div>
       <div class={styles.item}>D</div>
@@ -12,7 +26,7 @@ const Home = () => {
       <div class={styles.item}>F</div>
       <div class={styles.item}>G</div>
       <div class={styles.item}>H</div>
-      <div class={styles.item}>I</div>
+      <div class={styles.item}>I</div> */}
     </div>
   );
 };
