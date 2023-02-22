cd34737705e62a2b28a84dfbd92645657319a0d7
kyoongdev <9898junjun2@gmail.com>
2023-02-22 11:02:20 +0900

    feature : react_20230222

--- react_20230222/src/index.js
+++ react_20230222/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages";
 
 import reportWebVitals from "./reportWebVitals";
-import rootRouter from "./router";
 
 import "./styles/global.scss";
+//MEMO: SASS => SASS / SCSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={rootRouter} />
+    <HomePage />
   </React.StrictMode>
 );
 
